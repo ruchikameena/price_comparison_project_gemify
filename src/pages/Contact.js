6b1f586910456daaac35e8193835c952c4de1735
ruchikameena <ruchika01m@gmail.com>
2026-03-12 18:24:05 +0530

    contact.js removed as chatbot is used here

--- src/pages/Contact.js
+++
@@ -1,6 +0,0 @@
-// will gonna make this only if its required - optional here as chatbot is already present
-const Contact = () => {
-  return <h1>Contact Page displayed here</h1>;
-};
-
-export default Contact;
